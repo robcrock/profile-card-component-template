682ebd767944a5b6864be406b068a1bc40a07ba8
Robert Crocker <rcrocker13@gmail.com>
2024-07-14 17:59:26 -0500

    Added Attribution component to display challenge and coder information.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { Attribution } from "@/components/attribution";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos</div>
+      <Attribution
+        name={"Robert Crocker"}
+        link={"https://www.frontendmentor.io/profile/robcrock"}
+      />
     </main>
   );
 }
