4bb3ba9f34412fcb42500fdf43791e65aa5940be
Robert Crocker <rcrocker13@gmail.com>
2024-07-15 20:38:36 -0500

    Update page.tsx
    Made the attribution component more generic.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos</div>
       <Attribution
-        name={"Robert Crocker"}
-        link={"https://www.frontendmentor.io/profile/robcrock"}
+        name={"YOUR NAME HERE"}
+        link={"https://www.frontendmentor.io/profile/YOUR-PROFILE-NAME-HERE"}
       />
     </main>
   );
